274878dd8863d854d14c351ac4c5f61dac34494a
Lucas Nolte <lnolte@i.biz>
2012-08-17 08:51:56 +0200

    Inlined JS and Fonts so no network connection is required for the app

--- public/js/main.js
+++ public/js/main.js
@@ -1,101 +1,112 @@
 var Timer = {
-	value: 0,
-	seconds: 0,
-	minutes: 0,
-	hours: 0,
-	timing: false, // Indicating whether the timer is running
-
-	hoursDisplay: $('#hours span'),
-	minutesDisplay: $('#minutes span'),
-	secondsDisplay: $('#seconds span'),
-
-	start: function() {
-		// Prevent Timer from being started twice
-        if(this.timing === false) {
-            this.timing = true;
-            this.incrementTimer();
+value: 0,
+seconds: 0,
+minutes: 0,
+hours: 0,
+timing: false, // Indicating whether the timer is running
+    
+hoursDisplay: $('#hours span'),
+minutesDisplay: $('#minutes span'),
+secondsDisplay: $('#seconds span'),
+    
+start: function() {
+    // Prevent Timer from being started twice
+    if(this.timing === false) {
+        this.timing = true;
+        this.incrementTimer();
+    }
+},
+    
+incrementTimer: function() {
+    var _this = this;
+    
+    _this.value++;
+    
+    if(_this.seconds + 1 == 60) {
+        _this.seconds = 0;
+        if(_this.minutes + 1 == 60) {
+            _this.minutes = 0;
+            _this.hours++;
+            
+            macgap.growl.notify({
+                                title: "Timer",
+                                content: _this.hours + " hours completed!"
+                                });
+        } else {
+            _this.minutes++;
         }
-    },
-
-	incrementTimer: function() {
-		var _this = this;
-
-		_this.value++;
-
-			if(_this.seconds + 1 == 60) {
-				_this.seconds = 0;
-				if(_this.minutes + 1 == 60) {
-					_this.minutes = 0;
-					_this.hours++;
-                    
-                    macgap.growl.notify({
-                        title: "Timer",
-                        content: _this.hours + " hours completed!"
-                    });
-				} else {
-					_this.minutes++;
-				}
-			} else {
-				_this.seconds++;
-			}
-			
-			// Update the UI
-			_this.hoursDisplay.text(_this.hours);
-			_this.minutesDisplay.text(_this.minutes);
-			_this.secondsDisplay.text(_this.seconds);
-
-			if(_this.timing === true) {
-				setTimeout(function() {
-					if(_this.timing === true) {
-						_this.incrementTimer();
-					}
-				}, 1000);
-			}
-	},
-
-	pause: function() {
-		this.timing = false;
-	},
-
-	stop: function() {
-		this.timing = false;
-		this.reset();
-	},
-
-	reset: function() {
-		this.value = 0;
-		this.seconds = 0;
-		this.minutes = 0;
-		this.hours = 0;
-
-		this.hoursDisplay.text('0');
-		this.minutesDisplay.text('0');
-		this.secondsDisplay.text('0');
-	},
-
-	getValue: function() {
-		return this.value;
-	}
+    } else {
+        _this.seconds++;
+    }
+    
+    // Update the UI
+    _this.hoursDisplay.text(_this.hours);
+    _this.minutesDisplay.text(_this.minutes);
+    _this.secondsDisplay.text(_this.seconds);
+    
+    if(_this.timing === true) {
+        setTimeout(function() {
+                   if(_this.timing === true) {
+                   _this.incrementTimer();
+                   }
+                   }, 1000);
+    }
+},
+    
+pause: function() {
+    this.timing = false;
+},
+    
+stop: function() {
+    this.timing = false;
+    this.reset();
+},
+    
+reset: function() {
+    this.value = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+    this.hours = 0;
+    
+    this.hoursDisplay.text('0');
+    this.minutesDisplay.text('0');
+    this.secondsDisplay.text('0');
+},
+    
+getValue: function() {
+    return this.value;
+}
 };
 
 $(document).ready(function() {
-	$('#play').click(function() {
-        $('#controls i').removeClass('active');
-        $(this).addClass('active');
-        Timer.start();
-		return false;
-	});
-
-	$('#pause').click(function() {
-        $('#controls i').removeClass('active');
-        $(this).addClass('active');
-		Timer.pause();
-		return false;
-	});
-
-	$('#stop').click(function() {
-                     $('#controls i').removeClass('active');
-		Timer.stop();
-		return false;
-	});
-});
+                  $('#play').click(function() {
+                                   if(!$(this).hasClass('disabled')) {
+                                   $(this).addClass('disabled');
+                                   $('#controls i').removeClass('active');
+                                   $(this).addClass('active');
+                                   $('#pause, #stop').removeClass('disabled');
+                                   Timer.start();
+                                   }
+                                   return false;
+                                   });
+                  
+                  $('#pause').click(function() {
+                                    if(!$(this).hasClass('disabled')) {
+                                    $('#play').removeClass('disabled');
+                                    $('#controls i').removeClass('active');
+                                    $(this).addClass('active');
+                                    Timer.pause();
+                                    }
+                                    return false;
+                                    });
+                  
+                  $('#stop').click(function() {
+                                   if(!$(this).hasClass('disabled')) {
+                                   $('#controls i').removeClass('active');
+                                   $('#play').removeClass('disabled');
+                                   $('#pause, #stop').addClass('disabled');
+                                   Timer.stop();
+                                   }
+                                   return false;
+                                   });
+                  });
